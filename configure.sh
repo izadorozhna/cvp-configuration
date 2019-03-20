2ff48f1d2703a96745528381f984cb77d863f23b
Ievgeniia Zadorozhna <izadorozhna@mirantis.com>
2019-03-20 18:59:35 +0300

    Fixed intalling plugins in configure.sh
    For offline case we don't use Dockerfile, so installing plugins
    there is not matter for us. Added installing plugins from correct
    versions and sources in configure.sh.
    Note: using designate from izadorozhna repo.

--- configure.sh
+++ configure.sh
@@ -43,9 +43,9 @@ tempest_configuration () {
     apt-get update; apt-get install -y iputils-ping curl wget
     rally verify create-verifier --name tempest_verifier_$sub_name --type tempest --source $TEMPEST_REPO --version $tempest_version
     #rally verify add-verifier-ext --version 7a4bff728fbd8629ec211669264ab645aa921e2b --source https://github.com/openstack/telemetry-tempest-plugin
-    rally verify add-verifier-ext --version 12b770e923060f5ef41358c37390a25be56634f0 --source /var/lib/heat-tempest-plugin
-    rally verify add-verifier-ext --version 0.5.0 --source /var/lib/designate-tempest-plugin
-    rally verify add-verifier-ext --version stable/pike  --source /var/lib/neutron-lbaas
+    rally verify add-verifier-ext --version 12b770e923060f5ef41358c37390a25be56634f0 --source https://github.com/openstack/heat-tempest-plugin
+    rally verify add-verifier-ext --version mcp2019.2.0 --source https://github.com/izadorozhna/designate-tempest-plugin
+    rally verify add-verifier-ext --version stable/pike  --source https://github.com/openstack/neutron-lbaas
     pip install --force-reinstall python-cinderclient==3.2.0
     unset https_proxy
   fi
