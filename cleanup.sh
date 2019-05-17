89d1ad344ba3cd4670a6b0a2de35ab6ec4382e5d
Ievgeniia Zadorozhna <izadorozhna@mirantis.com>
2019-05-17 15:55:48 +0300

    Extend cleanup.sh script

--- cleanup.sh
+++ cleanup.sh
@@ -252,3 +252,113 @@ fi
 
 # remove temp file
 rm ${cmds}
+
+
+export OS_INTERFACE='admin'
+mask='rally_\|tempest_\|tempest-'
+echo "======================================================================="
+echo "Starting old clean-up script. Using mask '$mask'"
+
+echo "Part 1. Delete based on mask"
+
+echo "Delete users"
+for i in `openstack user list | grep $mask | awk '{print $2}'`; do openstack user delete $i; echo deleted $i; done
+
+echo "Delete roles"
+for i in `openstack role list | grep $mask | awk '{print $2}'`; do openstack role delete $i; echo deleted $i; done
+
+#echo "Delete projects"
+#for i in `openstack project list | grep $mask | awk '{print $2}'`; do openstack project delete $i; echo deleted $i; done
+
+echo "Delete servers"
+for i in `openstack server list --all | grep $mask | awk '{print $2}'`; do openstack server delete $i; echo deleted $i; done
+
+echo "Reset snapshot state and delete"
+for i in `openstack volume snapshot list --all | grep $mask | awk '{print $2}'`; do openstack snapshot set --state available $i; echo snapshot reset state is done for $i; done
+for i in `openstack volume snapshot list --all | grep $mask | awk '{print $2}'`; do openstack snapshot delete $i; echo deleted $i; done
+
+echo "Reset volume state and delete"
+for i in `openstack volume list --all | grep $mask | awk '{print $2}'`; do openstack volume set --state available $i; echo reset state is done for $i; done
+for i in `openstack volume list --all | grep $mask | awk '{print $2}'`; do openstack volume delete $i; echo deleted $i; done
+
+echo "Delete volume types"
+for i in `openstack volume type list | grep $mask | awk '{print $2}'`; do openstack volume type delete $i; done
+
+echo "Delete images"
+for i in `openstack image list | grep $mask | awk '{print $2}'`; do openstack image delete $i; echo deleted $i; done
+
+echo "Delete sec groups"
+for i in `openstack security group list --all | grep $mask | awk '{print $2}'`; do openstack security group delete $i; echo deleted $i; done
+
+echo "Delete keypairs"
+for i in `openstack keypair list | grep $mask | awk '{print $2}'`; do openstack keypair delete $i; echo deleted $i; done
+
+echo "Delete ports"
+for i in `openstack port list | grep $mask | awk '{print $2}'`; do openstack port delete $i; done
+
+echo "Delete Router ports (experimental)"
+neutron router-list|grep $mask|awk '{print $2}'|while read line; do echo $line; neutron router-port-list $line|grep subnet_id|awk '{print $11}'|sed 's/^\"//;s/\",//'|while read interface; do neutron router-interface-delete $line $interface; done; done
+
+echo "Delete subnets"
+for i in `openstack subnet list | grep $mask | awk '{print $2}'`; do openstack subnet delete $i; done
+
+echo "Delete nets"
+for i in `openstack network list | grep $mask | awk '{print $2}'`; do openstack network delete $i; done
+
+echo "Delete routers"
+for i in `openstack router list | grep $mask | awk '{print $2}'`; do openstack router delete $i; done
+
+echo "Delete regions"
+for i in `openstack region list | grep $mask | awk '{print $2}'`; do openstack region delete $i; echo deleted $i; done
+
+echo "Delete stacks"
+for i in `openstack stack list | grep $mask | awk '{print $2}'`; do openstack stack check $i; done
+for i in `openstack stack list | grep $mask | awk '{print $2}'`; do openstack stack delete -y $i; echo deleted $i; done
+
+echo "Delete containers"
+for i in `openstack container list --all | grep $mask | awk '{print $2}'`; do openstack container delete $i; echo deleted $i; done
+echo "Done"
+
+echo "Delete volume groups"
+for i in `cinder --os-volume-api-version 3.43  group-list --all-tenants 1 | grep $mask | awk '{print $2}'`; do cinder group-delete $i; done
+
+echo "Delete volume groups-types"
+for i in `cinder --os-volume-api-version 3.43  group-type-list | grep $mask | awk '{print $2}'`; do cinder group-type-delete $i; done
+
+echo "Delete sec groups"
+for i in `openstack security group list --all | grep $mask | awk '{print $2}'`; do openstack security group delete $i; echo deleted $i; done
+
+echo "Delete services"
+for i in `openstack service list | grep $mask | awk '{print $2}'`; do openstack service delete $i; echo deleted $i; done
+
+echo "Delete stacks (heat tempest plugin)"
+for i in `openstack stack list | grep "api-" | awk '{print $2}'`; do openstack stack check $i; done
+for i in `openstack stack list | grep "api-" | awk '{print $2}'`; do openstack stack delete -y $i; echo deleted $i; done
+
+echo "Delete DNS recorsd sets"
+for i in `designate domain-list --all | awk '{print $2}'`; do echo "==Workign with $i=="; for j in `openstack recordset list $i --all-projects | grep -v "SOA" | grep testdomain | awk '{print $2}'`; do openstack recordset delete $i $j --all-projects --edit-managed ; echo "deleted $j"; done; done
+
+echo "Delete DNS domains"
+for i in `designate domain-list --all | awk '{print $2}'`; do designate domain-delete $i --all; done
+
+
+echo "Tempest projects:"
+openstack project list | grep $mask
+
+echo "Part 2. Delete based on project id"
+
+for i in `openstack project list | grep $mask | awk '{print $2}'` ; do
+  echo "Starting. Using mask '$i'"
+  mask=$i
+
+  for j in `neutron lbaas-pool-list --all | grep $i | awk '{print $2}'`; do
+    for k in `neutron lbaas-member-list $j  | awk '{print $2}'`; do neutron lbaas-member-delete $k $j; done
+    neutron lbaas-pool-delete $j;
+  done
+  for x in `neutron lbaas-listener-list --all | grep $i | awk '{print $2}'`; do neutron lbaas-listener-delete $x; done
+  for c in `neutron lbaas-loadbalancer-list --all | grep $i | awk '{print $2}'`; do neutron lbaas-loadbalancer-delete $c; done
+
+  echo "Delete servers"
+  for i in `openstack server list --all | grep $mask | awk '{print $2}'`; do openstack server delete $i; echo deleted $i; done
+
+done
