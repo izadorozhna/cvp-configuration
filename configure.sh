62e2154b7d5043cbf02d89637078e38464acf60f
Ievgeniia Zadorozhna <izadorozhna@mirantis.com>
2019-12-16 19:11:20 +0300

    Removed telemetry plugin

--- configure.sh
+++ configure.sh
@@ -115,7 +115,6 @@ tempest_configuration () {
     rally verify add-verifier-ext --source /var/lib/heat-tempest-plugin
     rally verify add-verifier-ext --source /var/lib/designate-tempest-plugin
     rally verify add-verifier-ext --source /var/lib/neutron-lbaas
-    rally verify add-verifier-ext --source /var/lib/telemetry-tempest-plugin
     update_cacerts "/usr/local/lib"
   else
     if [ -n "${PROXY}" ]; then
@@ -133,8 +132,6 @@ tempest_configuration () {
     rally verify add-verifier-ext --version mcp/queens --source $current_path/designate-tempest-plugin
     # Install Neutron LBaaS plugin
     rally verify add-verifier-ext --version stable/queens --source https://github.com/openstack/neutron-lbaas
-    # Install Telemetry plugin
-    rally verify add-verifier-ext --version mcp/queens --source http://gerrit.mcp.mirantis.com/packaging/sources/telemetry-tempest-plugin
 
     pip install --force-reinstall python-cinderclient==3.2.0
     unset https_proxy
