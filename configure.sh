7639cbf55bc96e8b24564a2216c83e23feb9e43a
Ievgeniia Zadorozhna <izadorozhna@mirantis.com>
2024-11-27 11:51:26 +0400

    Disabled heat tempest plugin

--- configure.sh
+++ configure.sh
@@ -72,7 +72,7 @@ tempest_configuration () {
   if [ "$PROXY" == "offline" ]; then
     rally verify create-verifier --name tempest_verifier_$sub_name --type tempest --source $TEMPEST_REPO --system-wide --version $tempest_version
     #rally verify add-verifier-ext --source /var/lib/telemetry-tempest-plugin
-    rally verify add-verifier-ext --source /var/lib/heat-tempest-plugin
+    #rally verify add-verifier-ext --source /var/lib/heat-tempest-plugin
     update_cacerts "/usr/local/lib"
   else
     if [ -n "${PROXY}" ]; then
@@ -80,7 +80,7 @@ tempest_configuration () {
     fi
     rally verify create-verifier --name tempest_verifier_$sub_name --type tempest --source $TEMPEST_REPO --version $tempest_version
     #rally verify add-verifier-ext --version 7a4bff728fbd8629ec211669264ab645aa921e2b --source https://github.com/openstack/telemetry-tempest-plugin
-    rally verify add-verifier-ext --version 0.3.0 --source https://github.com/openstack/heat-tempest-plugin
+    #rally verify add-verifier-ext --version 0.3.0 --source https://github.com/openstack/heat-tempest-plugin
     pip install --force-reinstall python-cinderclient==3.2.0
     /home/rally/.rally/verification/*/.venv/bin/pip install --force-reinstall 'paramiko==2.7.2'
     unset https_proxy
